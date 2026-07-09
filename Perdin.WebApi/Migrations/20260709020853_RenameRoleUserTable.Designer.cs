97f7e5ececddfc7c31eabb8e36e6233cf8f4fa5a
nazladiohevin <nazlahevinbusiness@gmail.com>
2026-07-09 09:11:59 +0700

    feat: add permission

--- Perdin.WebApi/Migrations/20260709020853_RenameRoleUserTable.Designer.cs
+++ Perdin.WebApi/Migrations/20260709020853_RenameRoleUserTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Perdin.WebApi.Data;
 
@@ -11,9 +12,11 @@ using Perdin.WebApi.Data;
 namespace Perdin.WebApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260709020853_RenameRoleUserTable")]
+    partial class RenameRoleUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
