a27b6aac106b10e06853a2ab2ae054f1b4876f67
nazladiohevin <nazlahevinbusiness@gmail.com>
2026-07-09 16:22:24 +0700

    feat: add province CRUD

---
+++ Perdin.WebApi/DTOs/Province/ProvinceCreateRequest.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Perdin.WebApi.DTOs.Province
+{
+    public class ProvinceCreateRequest
+    {
+        [Required(ErrorMessage = "Nama provinsi wajib diisi.")]
+        [MinLength(3, ErrorMessage = "Nama provinsi minimal 3 karakter.")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "CountryId wajib diisi.")]
+        public int CountryId { get; set; }
+    }
+}
