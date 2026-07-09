9ab00381ae520f6d3d56b47056363c4ef6e290b4
nazladiohevin <nazlahevinbusiness@gmail.com>
2026-07-09 16:22:24 +0700

    feat: add province CRUD

---
+++ Perdin.WebApi/DTOs/Province/ProvinceResponse.cs
@@ -0,0 +1,11 @@
+namespace Perdin.WebApi.DTOs.Province
+{
+    public class ProvinceResponse
+    {
+        public int Id { get; set; }
+        public int CountryId { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
