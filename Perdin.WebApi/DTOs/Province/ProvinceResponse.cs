5a120f50afae5cd8210b86fc01d05c1b11cffaab
nazladiohevin <nazlahevinbusiness@gmail.com>
2026-07-13 22:15:32 +0700

    feat(api): add island mapping to province endpoints and sort by newest

--- Perdin.WebApi/DTOs/Province/ProvinceResponse.cs
+++ Perdin.WebApi/DTOs/Province/ProvinceResponse.cs
@@ -5,6 +5,7 @@ namespace Perdin.WebApi.DTOs.Province
         public int Id { get; set; }
         public int CountryId { get; set; }
         public string Name { get; set; } = null!;
+        public string Island { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
